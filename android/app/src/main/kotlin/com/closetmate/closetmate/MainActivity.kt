2c75886c8b07c50b0f6fdccb583eee410c7d43dc
codingwarren <codingwarren@gmail.com>
2026-04-17 10:08:05 +0800

    chore: android config - FlutterFragmentActivity, permissions, minSdk=21

--- android/app/src/main/kotlin/com/closetmate/closetmate/MainActivity.kt
+++ android/app/src/main/kotlin/com/closetmate/closetmate/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.closetmate.closetmate
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
